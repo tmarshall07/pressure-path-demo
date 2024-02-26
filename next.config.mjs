603c8825f8ff380033a3d111b87c45ad8e82909d
Tanner Marshall <tanner.s.marshall@gmail.com>
2024-02-26 16:55:46 -0500

    custom config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: { unoptimized: true },
+  experimental: { images: { unoptimized: true } },
+  basePath: '/docs',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
