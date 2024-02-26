6ea63b0392f207db98af48f3fdc4b88564afcc9d
Tanner Marshall <tanner.s.marshall@gmail.com>
2024-02-26 17:05:09 -0500

    Revert yml change

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: { unoptimized: true },
-  experimental: { images: { unoptimized: true } },
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'export',
 };
 
 export default nextConfig;
